5a4b11b3d3804ccc2b04e36121c01e2c344ced53
Stanley <sln95302@gmail.com>
2023-08-07 14:08:04 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -27,3 +27,22 @@ Hello!您好，還營您成為Master Finance的好友!
     line_bot_api.reply_message(
         event.reply_token,
         [text_message,sticker_message])
+    
+
+def push_msg(event,msg):
+    try:
+        user_id=event.sourcr.user_id
+        line_bot_api.push_message(user_id,TextSendMessage(text=msg))
+    except:
+        room_id=event.sourcr.room_id
+        line_bot_api.push_message(room_id,TextSendMessage(text=msg))
+
+def Usage(event):
+    push_msg(event,'✨✨ 查詢方法 ✨✨ \
+             \n\
+             \n\小幫手可以查詢油價 匯率 股價\
+             \n\
+             \n\油價通知 ➡️➡️➡️ 輸入可以查詢油價\
+             \n\匯率通知 ➡️➡️➡️ 輸入可以查詢匯率\
+             \n\匯率兌換 ➡️➡️➡️ 換匯USD/TWD\
+             \n\股價查詢 ➡️➡️➡️ 輸入#股票代號')
