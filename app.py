d803b39ac3d9dbe36f9fce558b5f883a9b581b50
Stanley <sln95302@gmail.com>
2023-08-09 10:16:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,12 @@
 from line_bot_api import*
 from events.basic import *
 from events.oil import *
+from events.Msg_Template import *
+import re
+import twstock
+import datetime
+
+
 app=Flask(__name__)
 
 
@@ -36,8 +42,10 @@ def handle_message(event):
     # message=TextSendMessage(text=event.message.text)
     # line_bot_api.reply_message(event.reply_token,message)
     profile=line_bot_api.get_profile(event.source.user_id)
-    uid=profile.user_id
+    uid=profile.user_id#使用者ID
     message_text=str(event.message.text).lower()
+    msg=str(event.message.text).upper().strip()#使用者輸入的內容
+    emsg=event.message.text
 
     if message_text=='@使用說明':
         about_us_event(event)
@@ -85,6 +93,41 @@ def handle_message(event):
     if event.message.text=='@股票查詢':
         line_bot_api.push_message(uid,TextSendMessage('請輸入#+股票代號....'))
 
+    #股價查詢
+    if re.match('想知道股價[0-9]',msg):
+        stockNumber=msg[2:6]
+        btn_msg=stock_reply_other(stockNumber)
+        line_bot_api.push_message(uid,btn_msg)
+        return 0
+    if (emsg.startswith('#')):
+        text=emsg[1:]
+        content=''
+
+        stock_rt=twstock.realtime.get(text)
+        my_datetime=datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+        my_time=my_datetime.strftime('%H:%M:%S')
+
+        content+= '%s(%s)%s\n'%(
+            stock_rt['info']['name'],
+            stock_rt['info']['code'],
+            my_time
+        )
+        content+='現價: %s / 開盤:%s\n'%(
+            stock_rt['realtime']['lateest_trade_price'],
+            stock_rt['realtime']['open']
+        )
+        content+='最高: %s / 最低:%s\n'%(
+            stock_rt['realtime']['high'],
+            stock_rt['realtime']['low']
+        )
+        content+='量: %s \n'%(stock_rt['realtime']['accumulate_trade_volume'])
+        stock=twstock.Stock(text)
+        content+='-----\n'
+        content+='最近五日價格: \n'
+        price5=stock.price[-5:][::-1]
+        date5=stock.date[-5:][::-1]
+        for i in range(len(price5)):
+            content+='[%s]%s\n'%(date5[i].strftime('%Y-%m-%d'),price5[i])
 
     @handler.add(FollowEvent)
     def handle_follow(event):
