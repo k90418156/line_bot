ff588a95abea74116eface3c45c4db4a3eefb178
Stanley <sln95302@gmail.com>
2023-08-09 10:30:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def handle_message(event):
         line_bot_api.push_message(uid,TextSendMessage('請輸入#+股票代號....'))
 
     #股價查詢
-    if re.match('想知道股價[0-9]',msg):
+    if re.match('想知道股價',msg):
         stockNumber=msg[2:6]
         btn_msg=stock_reply_other(stockNumber)
         line_bot_api.push_message(uid,btn_msg)
