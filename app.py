4337d00366a4b13147865968d90392d946fd80e8
Stanley <sln95302@gmail.com>
2023-08-09 10:20:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def handle_message(event):
             my_time
         )
         content+='現價: %s / 開盤:%s\n'%(
-            stock_rt['realtime']['lateest_trade_price'],
+            stock_rt['realtime']['latest_trade_price'],
             stock_rt['realtime']['open']
         )
         content+='最高: %s / 最低:%s\n'%(
