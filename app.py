521892036c11470d93eef2838945094a3c46d4bc
Stanley <sln95302@gmail.com>
2023-08-11 11:27:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -188,6 +188,12 @@ def handle_message(event):
     if re.match('幣別種類',emsg):
         message=show_Button()
         line_bot_api.reply_message(event.reply_token,message)
+
+    if re.match('查詢匯率[A-Z]{3}',msg):
+        msg=msg[4:]
+        content=showCurrency(msg)
+        line_bot_api.push_message(uid,TextSendMessage(content))
+
     if re.match('換匯[A-Z]{3}/[A-Z{3}]',msg):
         line_bot_api.push_message(uid,TextSendMessage('將為您做換匯計算....'))
         content=getExchangeRate(msg)
